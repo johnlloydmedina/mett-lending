b1155838431407d09bfbdeaa9965208e3e105a22
johnlloydmedina <127301351+johnlloydmedina@users.noreply.github.com>
2023-10-28 18:51:42 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Lending Management System</title>
+</head>
+<body>
+    <h1>Welcome to the Lending Management System</h1>
+
+    <ul>
+        <li><a href="borrower.php">Manage Borrowers</a></li>
+        <li><a href="loan.php">Manage Loans</a></li>
+        <li><a href="payment.php">Manage Payments</a></li>
+        <!-- Add more links for different features as needed -->
+    </ul>
+
+</body>
+</html>
